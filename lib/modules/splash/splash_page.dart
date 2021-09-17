60b338f424ce7ad0aabedfad8b022b2d1b0e7cfd
Vagner Bom Jesus <vagneripg@gmail.com>
2021-09-17 11:11:05 +0100

    configurar o firebase a adicionar o token da api no firebase

--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
@@ -9,7 +10,10 @@ class SplashPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Splash'),
       ),
-      body: Container(),
+      body: Center(
+          child: Container(
+        child: Text(RemoteConfig.instance.getString('api_token')),
+      )),
     );
   }
 }
