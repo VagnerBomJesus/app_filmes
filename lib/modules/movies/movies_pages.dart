9396710aaf36d47416dc3b8c4585cb9fc60e95be
Vagner Bom Jesus <vagneripg@gmail.com>
2021-09-20 10:37:35 +0100

    criacao do header da movies

--- lib/modules/movies/movies_pages.dart
+++ lib/modules/movies/movies_pages.dart
@@ -1,12 +1,21 @@
+import 'package:app_filmes/modules/movies/widgets/movies_header.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MoviesPage extends StatelessWidget {
   const MoviesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("Movies pages")),
+    return SizedBox(
+      width: Get.width,
+      child: ListView(
+        children: [
+          //for (int i = 0; i < 6; i++) Center(child: Text("data")),
+
+          MoviesHeader(),
+        ],
+      ),
     );
   }
 }
